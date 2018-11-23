619afa4877bc95d0784baeafc358348d89bff6ad
Jeol-an <rejoelve@gmail.com>
2018-11-24 01:07:27 +0900

    Mongoose 프로미스 설정 변경

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cboard', {
   useNewUrlParser: true
 });
+mongoose.Promise = global.Promise; 
  
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
