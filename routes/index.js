45e6dc0a01f86b7b78e99fc9cb989cc33c29526f
Jeol-an <rejoelve@gmail.com>
2018-11-25 09:29:32 +0900

    로그인후 유저세션정보 유지 및 로그아웃 구현

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 var passport = require('passport');
+
+function isLoggedIn(req) {
+  if (req.isAuthenticated()){
+      return req.user;
+  } else {
+      return "Guest";
+  }
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {  
+  res.render('index', { user: isLoggedIn(req) });
 });
 
 router.get('/login', function(req, res) {
-  res.render('login', { err: req.flash('err')});
+  res.render('login', { user: isLoggedIn(req), err: req.flash('err')});
 });
 
 router.get('/signup', function(req, res) {
-   res.render('signup', { err: req.flash('err')});
+   res.render('signup', { user: isLoggedIn(req), err: req.flash('err')});
+});
+
+router.get('/logout', function(req, res) {  
+  if(req.isAuthenticated()) {
+    req.session.destroy(function(err){
+      if(err) console.log(err);
+    });
+  }
+  
+  res.redirect('/');     
 });
 
 router.post('/signup', passport.authenticate('signup', {
